e615d3521f9fa7f87af4b11b9fd727e27d76ee59
Daniel <daniel.acaz@becommerce.com.br>
2020-06-25 12:33:40 -0300

    clean old files

--- src/main/java/br/com/mercadolivre/meliproxy/constant/ProxyParameter.java
+++
@@ -1,7 +0,0 @@
-package br.com.mercadolivre.meliproxy.constant;
-
-public enum ProxyParameter {
-
-    ORIGIN_IP, TARGET_PATH, ALL_PARAMS
-
-}
--- src/main/java/br/com/mercadolivre/meliproxy/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.mercadolivre.meliproxy.repository;
-
-import br.com.mercadolivre.meliproxy.model.RequestEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestRepository extends CrudRepository<RequestEntity, String> {
-}
