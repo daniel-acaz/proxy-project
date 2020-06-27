06a2a8fd8af3b043e6ca1147004a0828d5fbe072
Daniel <daniel.acaz@becommerce.com.br>
2020-06-27 00:51:43 -0300

    Create swagger and update pom to parent pom.xml

--- statistic/src/main/java/br/com/mercadolivre/statistic/StatisticApplication.java
+++ statistic/src/main/java/br/com/mercadolivre/statistic/StatisticApplication.java
@@ -3,9 +3,11 @@ package br.com.mercadolivre.statistic;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableSwagger2
 public class StatisticApplication {
 
 	public static void main(String[] args) {
